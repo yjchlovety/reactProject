904ba3c8686c562702a588f8abb57c06a7616d06
335758663@qq.com <liuzhengdong@xkeshi.com>
2016-10-13 16:53:02 +0800

    生产js更改

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -40,6 +40,7 @@ const config = {
       'process.env.NODE_ENV': JSON.stringify('production'),
       __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
     }),
+
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({ //js和css压缩
       compress: {
@@ -48,22 +49,16 @@ const config = {
       except: []    //排除关键字
     }),
     new HtmlWebpackPlugin({
+      favicon: './app/favicon.ico',
       template: 'app/template.html',
       filename: 'index.html',
-      inject: true
-    }),
-
-    // new HtmlWebpackPlugin({                        //根据模板插入css/js等生成最终HTML 压缩HTML
-    //   favicon: './src/img/favicon.ico', //favicon路径
-    //   filename: '/view/index.html',    //生成的html存放路径，相对于 path
-    //   template: './src/view/index.html',    //html模板路径
-    //   inject: true,    //允许插件修改哪些内容，包括head与body
-    //   hash: true,    //为静态资源生成hash值
-    //   minify: {    //压缩HTML文件
-    //     removeComments: true,    //移除HTML中的注释
-    //     collapseWhitespace: true    //删除空白符与换行符
-    //   }
-    // })
+      inject: true,  //允许插件修改哪些内容，包括head与body
+      hash: true,    //为静态资源生成hash值
+      minify: {    //压缩HTML文件
+        removeComments: true,    //移除HTML中的注释
+        collapseWhitespace: true    //删除空白符与换行符
+      }
+    })
   ]
 };
 
